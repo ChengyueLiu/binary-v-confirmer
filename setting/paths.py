b82215decdbe7f75257c6befe9f53e8d9c441008
ChengyueLiu <17554334529@163.com>
2024-03-08 16:59:07 +0800

    rename project

--- setting/paths.py
+++ setting/paths.py
@@ -1,4 +1,4 @@
 TREE_SITTER_LANGUAGE_FILE_PATH = "Resources/build/my-languages-mac.dll"
 IDA_PRO_PATH = r"C:\Users\liuchengyue\GreenPrograms\idapro75_51641\ida64.exe"
-IDA_PRO_SCRIPT_PATH = r"C:\Users\liuchengyue\Desktop\projects\Wroks\v-conformer\scripts\ida_scripts.py"
-IDA_PRO_OUTPUT_PATH = r"C:\Users\liuchengyue\Desktop\projects\Wroks\v-conformer\TestCases\feature_extraction\ida_pro_result.json"
+IDA_PRO_SCRIPT_PATH = r"C:\Users\liuchengyue\Desktop\projects\Wroks\binary-v-confirmer\scripts\ida_scripts.py"
+IDA_PRO_OUTPUT_PATH = r"C:\Users\liuchengyue\Desktop\projects\Wroks\binary-v-confirmer\TestCases\feature_extraction\ida_pro_result.json"
