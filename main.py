3c2d16999822d5a7748d780c1fe2cd6b73301294
ChengyueLiu <17554334529@163.com>
2024-03-22 14:49:45 +0800

    update

--- main.py
+++ main.py
@@ -174,9 +174,9 @@ def test_model():
     libssl_321 = "TestCases/binaries/openssl_3.2.1/libssl.so.3"
     libpng16 = "TestCases/binaries/libpng/libpng16.so.16.43.0"
 
-    # save_path = "test_results/openssl_320_result.json"
-    # vul_confirm_team.confirm(binary_path=openssl_320, vul=vulnerability)
-    # save_to_json_file(vulnerability.customer_serialize(), save_path, output_log=True)
+    save_path = "test_results/openssl_320_result.json"
+    vul_confirm_team.confirm(binary_path=openssl_320, vul=vulnerability)
+    save_to_json_file(vulnerability.customer_serialize(), save_path, output_log=True)
 
     save_path = "test_results/libcrypto_320_result.json"
     vul_confirm_team.confirm(binary_path=libcrypto_320, vul=vulnerability)
@@ -186,9 +186,9 @@ def test_model():
     vul_confirm_team.confirm(binary_path=libcrypto_321, vul=vulnerability)
     save_to_json_file(vulnerability.customer_serialize(), save_path, output_log=True)
 
-    # save_path = "test_results/libpng_16_result.json"
-    # vul_confirm_team.confirm(binary_path=libpng16, vul=vulnerability)
-    # save_to_json_file(vulnerability.customer_serialize(), save_path, output_log=True)
+    save_path = "test_results/libpng_16_result.json"
+    vul_confirm_team.confirm(binary_path=libpng16, vul=vulnerability)
+    save_to_json_file(vulnerability.customer_serialize(), save_path, output_log=True)
 
 
 
