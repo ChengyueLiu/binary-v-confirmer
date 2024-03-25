c16ab6b01d10f71ad2b8d1c5ae81928ac57b2820
ChengyueLiu <17554334529@163.com>
2024-03-25 15:38:10 +0800

    update requirements.txt

--- generate_tree-sitter_languages.py
+++ generate_tree-sitter_languages.py
@@ -11,6 +11,7 @@ from tree_sitter import Language
 
 from setting.paths import TREE_SITTER_LANGUAGE_FILE_PATH
 
+# When using on windows, Microsoft Visual C++ 14.0 or greater is required. Get it with "Microsoft C++ Build Tools": https://visualstudio.microsoft.com/visual-cpp-build-tools/
 Language.build_library(
     # Store the library in the `build` directory
     # 'resources/build/my-languages-mac.so',
--- setting/paths.py
+++ setting/paths.py
@@ -3,7 +3,9 @@ from environs import Env
 env = Env()
 env.read_env()
 
-TREE_SITTER_LANGUAGE_FILE_PATH = env.str("TREE_SITTER_LANGUAGE_FILE_PATH", "Resources/build/my-languages-mac.dll")
-IDA_PRO_PATH = r"C:\Users\liuchengyue\GreenPrograms\idapro75_51641\ida64.exe"
-IDA_PRO_SCRIPT_PATH = r"C:\Users\liuchengyue\Desktop\projects\Wroks\binary-v-confirmer\scripts\ida_scripts.py"
-IDA_PRO_OUTPUT_PATH = r"C:\Users\liuchengyue\Desktop\projects\Wroks\binary-v-confirmer\TestCases\feature_extraction\ida_pro_result.json"
+TREE_SITTER_LANGUAGE_FILE_PATH = env.str("TREE_SITTER_LANGUAGE_FILE_PATH", "Resources/build/my-languages-win.dll")
+IDA_PRO_PATH = env.str("IDA_PRO_PATH", r"C:\Users\chengyue\Desktop\GreenPrograms\idapro75_51641\ida64.exe")
+IDA_PRO_SCRIPT_PATH = env.str("IDA_PRO_SCRIPT_PATH",
+                              r"C:\Users\chengyue\Desktop\projects\binary-v-confirmer\scripts\ida_scripts.py")
+IDA_PRO_OUTPUT_PATH = env.str("IDA_PRO_OUTPUT_PATH",
+                              r"C:\Users\chengyue\Desktop\projects\binary-v-confirmer\TestCases\feature_extraction\ida_pro_result.json")
