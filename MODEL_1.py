9c511b326c7520cb6ea558828e4d87c0c6f9b7c8
ChengyueLiu <17554334529@163.com>
2024-03-28 17:01:22 +0800

    optimize model 1 data prepare

--- MODEL_1.py
+++ MODEL_1.py
@@ -23,12 +23,17 @@ libpng_src_dir = r"C:\Users\chengyue\Desktop\projects\github_projects\libpng"
 # lib/：一些基本库函数。
 linux_kernel_dir = r"C:\Users\chengyue\Desktop\projects\github_projects\linux-6.8.2_for_extraction"
 
-# bin
-libcrypto_bin_path = r"TestCases/binaries/openssl_3.2.1/libcrypto.so.3"
-openssl_bin_path = r"TestCases/binaries/openssl_3.2.1/openssl"
-libssl_bin_path = r"TestCases/binaries/openssl_3.2.1/libssl.so.3"
-libpng_src_path = r"C:\Users\liuchengyue\Desktop\projects\GithubProjects\libpng"
+# debian bin
+libcrypto_bin_path = r"TestCases/binaries/debian/openssl_3.2.1/libcrypto.so.3"
+openssl_bin_path = r"TestCases/binaries/debian/openssl_3.2.1/openssl"
+libssl_bin_path = r"TestCases/binaries/debian/openssl_3.2.1/libssl.so.3"
+libpng_bin_path = r"TestCases/binaries/debian/libpng/libpng16.so.16.43.0"
 linux_kernel_path = "TestCases/binaries/linux_kernel_6.8.2/vmlinux"
+
+# self compiled bin
+O0_libcrypto_bin_path = r"TestCases/binaries/self_compiled/opensl_3.2.1/O0/libcrypto.so.3"
+O0_openssl_bin_path = r"TestCases/binaries/self_compiled/opensl_3.2.1/O0/openssl"
+O0_libssl_bin_path = r"TestCases/binaries/self_compiled/opensl_3.2.1/O0/libssl.so.3"
 
 # openssl matched_function_feature
 openssl_function_features_path = r"TestCases/feature_extraction/openssl_feature/function_features.json"
@@ -49,13 +54,20 @@ def prepare_train_data_for_model_1():
     2. 把函数特征转换成训练数据
     :return:
     """
-
+    # debian bin
     src_bin_pairs = [
-        (libcrypto_src_dir, libcrypto_bin_path),
-        (openssl_src_dir, openssl_bin_path),
-        (libssl_src_dir, libssl_bin_path),
-        (libpng_src_dir, libssl_bin_path),
+        (libcrypto_src_dir, libcrypto_bin_path),  # openssl O2 3.2.1
+        (openssl_src_dir, openssl_bin_path),  # openssl O2 3.2.1
+        (libssl_src_dir, libssl_bin_path),  # openssl O2 3.2.1
+        (libpng_src_dir, libpng_bin_path),  # libpng O2 16.43
         # (linux_kernel_dir, linux_kernel_path),
+    ]
+
+    # self_compiled bin
+    src_bin_pairs = [
+        (libcrypto_src_dir, O0_libcrypto_bin_path),  # openssl O0 3.2.1
+        (openssl_src_dir, O0_openssl_bin_path),  # openssl O0 3.2.1
+        (libssl_src_dir, O0_libssl_bin_path),  # openssl O0 3.2.1
     ]
 
     # 提取函数特征, 注意，这里有筛选，只提取长度在7-100之间的函数
@@ -63,30 +75,26 @@ def prepare_train_data_for_model_1():
         src_bin_pairs=src_bin_pairs,
         save_path=openssl_function_features_path,
     )
+
     # 转换成训练数据 1:5, 0.2的相似度，效果很好 97%准确率
     """
-    多进行几次实验，争取找到最好的训练样本比例和相似度阈值
+    多进行几次实验，争取找到最好的训练样本比例和相似度阈值,以下实验结果，除特殊注明外，都是在debian二进制上实验的
     正负1:1   0.2的相似度阈值，
     正负1:1   0.5的相似度阈值，
-    正负1：3   0.2的相似度阈值，
-    正负1：3   0.5的相似度阈值，正在测试中
-    正负1:5   0.2的相似度阈值，97%准确率
+    正负1:3   0.2的相似度阈值，94%
+    正负1:3   0.5的相似度阈值，94%，但是过拟合的比0.2差一点，应该是能够学习到更多的细节
+    正负1:5   0.2的相似度阈值，97%
     正负1:5   0.5的相似度阈值，
-    正负1:10  0.5的相似度阈值。
+    正负1:10  0.2的相似度阈值，
+    正负1:10  0.5的相似度阈值，97.8%
+    正负1:10  0.5的相似度阈值，（自己编译的O0级别），正在测试中
     """
     convert_function_feature_to_train_data(openssl_function_features_path,
                                            train_data_save_path,
                                            val_data_save_path,
                                            test_data_save_path,
-                                           negative_ratio=3,
+                                           negative_ratio=10,
                                            similarity_threshold=0.5)
-
-    # linux_kernel train data
-    # convert_function_feature_to_train_data(openssl_function_features_path,
-    #                                        linux_kernel_train_data_save_path,
-    #                                        linux_kernel_val_data_save_path,
-    #                                        linux_kernel_test_data_save_path,
-    #                                        negative_ratio=5)
 
 
 def train_model_1():
