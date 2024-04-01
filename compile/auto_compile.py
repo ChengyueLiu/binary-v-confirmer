a79b9ab7b59eac867040de7006034d5de9c4dd9e
ChengyueLiu <17554334529@163.com>
2024-04-01 10:59:03 +0800

    add auto_compile

--- compile/auto_compile.py
+++ compile/auto_compile.py
@@ -1,15 +1,15 @@
 from bintools.general.file_tool import find_files_in_dir
 
 
-def compile():
+def compile_file(file_path, binary_path):
     pass
 
 
-def objdump():
+def objdump(binary_path, objdump_path):
     pass
 
 
-def extract_asm():
+def extract_asm(objdump_path, asm_path):
     pass
 
 
@@ -18,4 +18,13 @@ def main():
     binary_dir = 'bin'
 
     file_paths = find_files_in_dir(source_dir)
-    pass
+    for path in file_paths:
+        for compiler in ['gcc', 'clang']:
+            for opt in ['O0', 'O1', "O2", "O3"]:
+
+                binary_path = binary_dir + '/' + path + '_' + compiler + '_' + opt
+                compile_file(path, binary_path)
+                objdump_path = binary_path + '.objdump'
+                objdump(binary_path, objdump_path)
+                asm_path = binary_path + '.asm'
+                extract_asm(objdump_path, asm_path)
