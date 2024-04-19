70786f7af56a75f3a9d710f24ad2d0a819c5a258
ChengyueLiu <17554334529@163.com>
2024-04-19 13:33:58 +0800

    update Experiment

--- Experiment.py
+++ Experiment.py
@@ -30,7 +30,6 @@ def load_test_cases(tc_save_path) -> List[VulConfirmTC]:
     test_cases = [VulConfirmTC.init_from_dict(tc) for tc in test_cases]
     return test_cases
 
-
 def generate_model_input(asm_function, vul_function):
     # 构成模型输入
     asm_codes, _ = asm_function.get_asm_codes()
@@ -80,28 +79,12 @@ def confirm_functions(model, tc: VulConfirmTC, asm_functions_cache: dict):
 
     # 1. 提取汇编函数
     logger.info(f"\textracting asm functions from {test_bin.binary_path}")
-    if test_bin.binary_path not in asm_functions_cache:
-        asm_function_dict = parse_objdump_file(test_bin.binary_path, ignore_warnings=True)
-        asm_functions_cache[test_bin.binary_path] = asm_function_dict
-    else:
-        asm_function_dict = asm_functions_cache[test_bin.binary_path]
+    asm_function_dict = extract_asm_functions(asm_functions_cache, test_bin)
     logger.info(f"\t\textracted {len(asm_function_dict)} asm functions")
 
     # 2. 过滤asm函数并生成模型输入数据
     logger.info(f"\t\tfilter asm functions and generating model input data...")
-    found_functions = []
-    data_items = []
-    tasks = [(asm_function, vul_function)
-             for vul_function in vul_functions
-             for asm_function in asm_function_dict.values()]
-    with Pool() as pool:
-        results = pool.imap_unordered(generate_model_input_wrapper, tasks)
-        for data_item in results:
-            if data_item is None:
-                continue
-            data_items.append(data_item)
-            if data_item.function_name.strip("*") == data_item.bin_function_name:
-                found_functions.append(data_item.bin_function_name)
+    data_items, found_functions = filter_and_generate_data_items(asm_function_dict, vul_functions)
     logger.info(f"\t\tgenerated {len(data_items)} data items")
     logger.info(f"\t\tvul functions: {[vf.function_name for vf in vul_functions]}")
     logger.info(f"\t\tvul functions in data items: {found_functions}")
@@ -150,6 +133,32 @@ def confirm_functions(model, tc: VulConfirmTC, asm_functions_cache: dict):
         logger.info(f"\t\t{asm_codes}")
     logger.info(f"\tconfirm result: {confirmed_function_name} {confirmed_prob}")
     return confirmed_function_name, confirmed_prob
+
+
+def filter_and_generate_data_items(asm_function_dict, vul_functions):
+    found_functions = []
+    data_items = []
+    tasks = [(asm_function, vul_function)
+             for vul_function in vul_functions
+             for asm_function in asm_function_dict.values()]
+    with Pool() as pool:
+        results = pool.imap_unordered(generate_model_input_wrapper, tasks)
+        for data_item in results:
+            if data_item is None:
+                continue
+            data_items.append(data_item)
+            if data_item.function_name.strip("*") == data_item.bin_function_name:
+                found_functions.append(data_item.bin_function_name)
+    return data_items, found_functions
+
+
+def extract_asm_functions(asm_functions_cache, test_bin):
+    if test_bin.binary_path not in asm_functions_cache:
+        asm_function_dict = parse_objdump_file(test_bin.binary_path, ignore_warnings=True)
+        asm_functions_cache[test_bin.binary_path] = asm_function_dict
+    else:
+        asm_function_dict = asm_functions_cache[test_bin.binary_path]
+    return asm_function_dict
 
 
 def check_result(tc, confirmed_function_name, analysis):
@@ -229,10 +238,10 @@ def run_experiment():
     # # 包含，且已修复
     # test_case = [tc for tc in test_cases
     #              if tc.ground_truth.contained_vul_function_names and tc.ground_truth.is_fixed]
-
+    test_cases = test_cases[:100]
     asm_functions_cache = {}
     analysis = Analysis()
-    for i, tc in enumerate(test_cases[:100], 1):
+    for i, tc in enumerate(test_cases, 1):
         logger.info(f"confirm: {i} {tc.public_id}")
         confirmed_function_name, confirmed_prob = confirm_functions(model, tc, asm_functions_cache)
         check_result(tc, confirmed_function_name, analysis)
